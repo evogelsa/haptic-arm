d4fb9b51183fafea9d5791ec474033a6eca83430
Ethan Vogelsang <evogelsa@iu.edu>
2019-03-22 15:52:22 -0400

    Pos to graph & tweaks

--- odrive/odrvplot.py
+++ odrive/odrvplot.py
@@ -2,6 +2,7 @@ from os.path import isfile, isdir
 from os import mkdir
 import math
 import time
+import numpy as np
 import matplotlib.pyplot as plot
 import matplotlib.animation as animation
 from matplotlib import style
@@ -20,7 +21,7 @@ def torqueSine(torquefile, t0, odrv):
    # get time difference since start of program
    t = time.monotonic() - t0
    # calculate a voltage based on sine wave
-   voltage = 10.0 * math.sin(t)
+   voltage = 5.0 * math.sin(np.pi*t) + 5
    # set odrv to calculated voltage
    odrv.axis0.controller.current_setpoint = voltage
    # calculate amps
@@ -38,13 +39,26 @@ def torqueSine(torquefile, t0, odrv):
    print("%-9f | %-9f | %-9f | %-9f" %(t, voltage, I, T))
 
 
-# calcInputs(data_file_path)
+# calcPos(data_file_path)
+#
+# this function writes the position in degrees to a data file provided in args
+#
+def calcPos(posfile, odrv):
+   pos = odrv.axis0.encoder.count_in_cpr
+   pos = pos / 8192 * 360
+   val = str(pos) + '\n'
+   data = open(posfile, 'a')
+   data.write(val)
+   data.close()
+
+
+# calcWA(data_file_path)
 #
 # this function takes a path to a data file and writes two values for an
 # estimated angular velocity in rad/s and an estimated angular acceleration
 # from those two velocities.
 #
-def calcInputs(alphafile, odrv):
+def calcWA(alphafile, odrv):
    # make list of size 2 to store angular vel
    omegas = [0,0]
    for _ in range(2):
@@ -87,6 +101,7 @@ def plotSetup():
    ax1 = fig.add_subplot(1,1,1)
    # make second axis with same x axis as ax1
    ax2 = ax1.twinx()
+   ax3 = ax1.twinx()
    # adjust the padding of plots
    plot.subplots_adjust(left=.140,right=.84,bottom=.14,top=.92)
    # check for logs folder, if not exist make it
@@ -128,12 +143,31 @@ def plotSetup():
       data = open(torquefile,'w')
       data.write('0,0,0\n')
       data.close()
+   # check if pout0 exists and if it does create file with correct number
+   if isfile('logs/pout0.txt'):
+      n = 1
+      # iterate through files 0 to x until x doesn't exist
+      while n > 0:
+         posfile = 'logs/pout' + str(n) + '.txt'
+         if isfile(posfile):
+            n += 1
+         else:
+            data = open(posfile,'w')
+            data.write('0.0\n')
+            data.close()
+            n = 0
+   else:
+      posfile = 'logs/pout0.txt'
+      data = open(torquefile,'w')
+      data.write('0.0\n')
+      data.close()
    # make some empty arrays
    X = []
    Y = []
    A = []
+   P = []
 
-   return fig, ax1, ax2, torquefile, X, Y, A, alphafile
+   return fig, ax1, ax2, torquefile, X, Y, A, alphafile, posfile, P, ax3
 
 
 # animate(interval, float_array_time, float_array_torque, float_array_alpha)
@@ -144,10 +178,11 @@ def plotSetup():
 # amount of data trimemed * the interval of measurements. I.e. measurements 
 # every 10 ms * 200 data points = 2 seconds of data shown on the plot
 # 
-def animate(i,t0,X,Y,A,torquefile,alphafile,odrv,ax1,ax2):
+def animate(i,t0,X,Y,A,torquefile,alphafile,odrv,ax1,ax2,posfile,P,ax3):
    # run torque sine to generate new point and write to file
    torqueSine(torquefile, t0, odrv)
-   calcInputs(alphafile, odrv)
+   calcWA(alphafile, odrv)
+   calcPos(posfile, odrv)
    # read the file to see the new data
    data = open(torquefile,'r').read()
    # split each coord with \n
@@ -171,13 +206,25 @@ def animate(i,t0,X,Y,A,torquefile,alphafile,odrv,ax1,ax2):
          # split vals with ,
          omega1, omega2, alpha = line.split(',')
          A.append(float(alpha))
+   # read the pos file to see the new data
+   data = open(posfile,'r').read()
+   # split each coord with \n
+   lines = data.split('\n')
+   # make empty data arrays
+   for line in lines:
+   # check that line isnt empty
+      if len(line) > 1:
+         pos = line
+         P.append(float(pos))
    # limit X and Y and A to the last 2 seconds of data
-   Y = Y[-200:]
-   X = X[-200:]
-   A = A[-200:]
+   Y = Y[-20:]
+   X = X[-20:]
+   A = A[-20:]
+   P = P[-20:]
    # clear the old stuff from graph
    ax1.clear() 
    ax2.clear()
+   ax3.clear()
    # set current axis to ax1
    plot.sca(ax1)
    # draw new data onto plot and round to four decimal places
@@ -190,7 +237,13 @@ def animate(i,t0,X,Y,A,torquefile,alphafile,odrv,ax1,ax2):
    # set current axis to ax2
    plot.sca(ax2)
    plot.plot(color='r')
-   ax2.plot(X,[round(a, 4) for a in A], color='red',linewidth=0.8)
+   ax2.plot(X,[round(p,4) for p in P], color='red',linewidth=0.8)
+   plot.ylim((-500,500))
+   plot.ylabel('Pos (deg)')
+   # set current axis to ax3
+   plot.sca(ax3)
+   plot.plot(color='g')
+   ax3.plot(X,[round(a,4) for a in A], color='green',linewidth=0.8)
    plot.ylim((-350,350))
    plot.ylabel('Angular Acceleration (rads/s/s)')
 
@@ -248,14 +301,14 @@ def main():
    # setup ODrive
    odrv = odrvSetup()
    # setup plot
-   fig, ax1, ax2, torquefile, X, Y, A, alphafile = plotSetup()
+   fig, ax1, ax2, torquefile, X, Y, A, alphafile, posfile, P, ax3 = plotSetup()
    # set our starting time to current sys time, used for calculating time passed
    t0 = time.monotonic()
    print("Performing sine wave...")
    # print formatted headers 
    print("%-9s | %-9s | %-9s | %-9s" %(" Time","Volts","Current","Torque"))
    # animation function takes figure to use, func for animation, update interval 
-   ani = animation.FuncAnimation(fig,animate,fargs=(t0,X,Y,A,torquefile,alphafile,odrv,ax1,ax2),interval=20)
+   ani = animation.FuncAnimation(fig,animate,fargs=(t0,X,Y,A,torquefile,alphafile,odrv,ax1,ax2,posfile,P,ax3),interval=10)
    # show our plot
    plot.show()
 
