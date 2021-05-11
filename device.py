916fde2a63635b3a3aa804313cfbe8cd621f5af2
Ethan Vogelsang <ethanvogelsang@gmail.com>
2021-05-11 16:04:25 -0400

    make analytical and numerical IK consistent with each other

--- device.py
+++ device.py
@@ -12,10 +12,10 @@ from odrive.enums import (
 
 
 class HapticDevice():
-    '''
+    """
     Wrapper for the SCARA arm and its associated utility functions. Stores
     objects for each arm segment and the odrive controller
-    '''
+    """
     # TODO
     # [ ] create @property and @setter methods for arm angles
     # [ ] " " for x, y
@@ -26,9 +26,9 @@ class HapticDevice():
         self.arm1 = ArmSegment(arm_length)
 
     def calibrate(self, axes=[0,1]):
-        '''
+        """
         Run the odrive axis/encoder calibration routinen on the specified axes
-        '''
+        """
         if 0 in axes:
             print('Calibrating axis 0')
             self.odrive.axis0.requested_state = \
@@ -43,10 +43,10 @@ class HapticDevice():
                 sleep(.1)
 
     def home(self):
-        '''
+        """
         Run the homing routine to calculate zero position and equivalent encoder
         counts to radians/degrees. In the future can be automated with endstops
-        '''
+        """
         # move to zero position and record
         input("Move arm to first position and press enter to continue")
         self.arm0.zero = self.odrive.axis0.encoder.shadow_count
@@ -71,10 +71,10 @@ class HapticDevice():
         print(self.arm0.cnt_per_rad, self.arm1.cnt_per_rad)
 
     def set_axis_state_closed_loop(self, axes=[0,1]):
-        '''
+        """
         Set the specified axes to closed loop control mode, requires
         calibration. Automatically called when setting control modes
-        '''
+        """
         if 0 in axes:
             if (self.odrive.axis0.motor.is_calibrated and
                 self.odrive.axis0.encoder.is_ready):
@@ -93,10 +93,10 @@ class HapticDevice():
                                 + ' it has not been calibrated')
 
     def set_ctrl_mode_torque(self, axes=[0,1]):
-        '''
+        """
         Set the specified axes to torque control mode and subsequently set to
         closed loop control
-        '''
+        """
         if 0 in axes:
             self.odrive.axis0.controller.config.control_mode = \
                 CONTROL_MODE_TORQUE_CONTROL
@@ -106,10 +106,10 @@ class HapticDevice():
         self.set_axis_state_closed_loop(axes)
 
     def set_ctrl_mode_position(self, axes=[0,1]):
-        '''
+        """
         Set the specified axes to position control mode and subsequently set to
         closed loop control
-        '''
+        """
         if 0 in axes:
             self.odrive.axis0.controller.config.control_mode = \
                 CONTROL_MODE_POSITION_CONTROL
@@ -119,10 +119,10 @@ class HapticDevice():
         self.set_axis_state_closed_loop(axes)
 
     def set_ctrl_mode_velocity(self, axes=[0,1]):
-        '''
+        """
         Set the specified axes to velocity control mode and subsequently set to
         closed loop control
-        '''
+        """
         if 0 in axes:
             self.odrive.axis0.controller.config.control_mode = \
                 CONTROL_MODE_VELOCITY_CONTROL
@@ -132,9 +132,9 @@ class HapticDevice():
         self.set_axis_state_closed_loop(axes)
 
     def hold(self, axes=[0,1]):
-        '''
+        """
         Changes control mode to position and makes motors hold current position
-        '''
+        """
         self.set_ctrl_mode_position(axes)
         if 0 in axes:
             self.odrive.axis0.controller.input_pos = \
@@ -144,40 +144,51 @@ class HapticDevice():
                 self.odrive.axis1.encoder.shadow_count
 
     def restart(self):
-        '''
+        """
         Reboots the associated odrive controller
-        '''
+        """
         self.odrive.reboot()
 
     def fwd_kinematics(self, theta0: float, theta1: float) -> tuple[float]:
-        '''
+        """
         Calculate the end effector position in cartesian coordinates from the
         given arm configuration
-        '''
+        """
         x = self.arm0.length*np.cos(theta0) + self.arm1.length*np.cos(theta1)
         y = self.arm0.length*np.sin(theta0) + self.arm1.length*np.sin(theta1)
         return x, y
 
-    def inv_kinematics(self, x, y) -> tuple[float]:
-        '''
+    def inv_kinematics(self, x, y) -> np.ndarray:
+        """
         Calcultes the elbow down configuration required to achieve the given
         end effector position. Returns a tuple joint angles in radians
-        '''
+        """
         eps = np.finfo(float).eps
         rsq = x**2 + y**2
+        r = np.sqrt(rsq)
+        l0 = self.arm0.length
+        l1 = self.arm1.length
 
-        alpha = np.arccos((rsq + (self.arm0.length**2 - self.arm1.length**2))
-                          / (2 * self.arm0.length * np.sqrt(rsq) + eps))
-        beta  = np.arccos((self.arm0.length**2 + self.arm1.length**2 - rsq)
-                          / (2 * self.arm0.length * self.arm1.length))
+        alpha = np.arccos((rsq + (l0**2 - l1**2)) / (2 * l0 * r + eps))
+        beta = np.arccos(((l0**2 + l1**2) - rsq) / (2 * l0 * l1))
         gamma = np.arctan2(y, x)
 
+        #  alpha = np.arccos((rsq + (self.arm0.length**2 - self.arm1.length**2))
+        #                    / (2 * self.arm0.length * np.sqrt(rsq) + eps))
+        #  beta  = np.arccos((self.arm0.length**2 + self.arm1.length**2 - rsq)
+        #                    / (2 * self.arm0.length * self.arm1.length))
+        #  gamma = np.arctan2(y, x)
+
         theta0 = gamma - alpha
-        theta1 = beta - theta0
+        theta1 = np.pi - beta
 
-        return theta0, theta1
+        theta1 += theta0
 
-    def inv_kinematics_num(self, x, y, tol=0.005, maxdepth=20) -> tuple[float]:
+        thetas = np.asarray([theta0, theta1])
+
+        return thetas
+
+    def inv_kinematics_num(self, x, y, tol=0.005, maxdepth=20) -> np.ndarray:
         # goal position
         # tols - diff between prev guess and current guess
 
@@ -188,10 +199,7 @@ class HapticDevice():
             # initial guess - use arm's home config
             thetas_old = np.array((-np.pi/4, np.pi/2))
 
-        #  thetas = np.array((float('inf'), float('inf')))
-        #
-        #  diff = thetas - thetas_old
-        #  diff = max(diff)
+        thetas = thetas_old
 
         pd = np.array([x, y])
         diff = float('inf')
@@ -215,25 +223,25 @@ class HapticDevice():
 
         return thetas
 
-    def jacobian(self, theta0: float, theta1: float) -> list[float]:
-        '''
+    def jacobian(self, theta0: float, theta1: float) -> np.ndarray:
+        """
         Given two arm angles in radians, return the jacobian matrix
-        '''
+        """
         return np.array([[-self.arm0.length * np.sin(theta0),
                           -self.arm1.length * np.sin(theta1)],
                          [self.arm0.length * np.cos(theta0),
                           self.arm1.length * np.cos(theta1)]])
 
-    def inv_jacobian(self, theta0: float, theta1: float) -> list[float]:
-        '''
+    def inv_jacobian(self, theta0: float, theta1: float) -> np.ndarray:
+        """
         Given two arm angles in radians, return the inverse jacobian
-        '''
+        """
         return np.linalg.pinv(self.jacobian(theta0, theta1))
 
     def rad2count(self, theta: float, axis: int) -> int:
-        '''
+        """
         Convert radians to encoder counts
-        '''
+        """
         try:
             if axis == 0:
                 return theta * self.arm0.cnt_per_rad + self.arm0.zero
@@ -244,9 +252,9 @@ class HapticDevice():
             raise
 
     def count2rad(self, count: int, axis: int) -> float:
-        '''
+        """
         Convert encoder counts to radians
-        '''
+        """
         try:
             if axis == 0:
                 return (count - self.arm0.zero) / self.arm0.cnt_per_rad
@@ -257,10 +265,10 @@ class HapticDevice():
             raise
 
     def get_config(self) -> tuple[float]:
-        '''
+        """
         Returns the current arm configuration as a tuple of joint angles
         in radians
-        '''
+        """
         count0 = self.odrive.axis0.encoder.shadow_count
         count1 = self.odrive.axis1.encoder.shadow_count
 
@@ -270,9 +278,9 @@ class HapticDevice():
         return theta0, theta1
 
 class ArmSegment():
-    '''
+    """
     Wrapper for data associated to each arm segment of the parent device.
-    '''
+    """
     def __init__(self, length=0.2):
         self.length = length
         self.zero = None
