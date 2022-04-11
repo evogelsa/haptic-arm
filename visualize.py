9f1f7dee2aec67531ad7ec0ff7b52f98115c19ed
Ethan Vogelsang <ethanvogelsang@gmail.com>
2022-04-11 18:11:24 -0400

    format with black

--- visualize.py
+++ visualize.py
@@ -253,7 +253,7 @@ class SDLWrapper:
         #  print('numerical:', arm.inv_kinematics_num(0, 0.4))
 
         binsz = 1
-        num_samples = win_width * win_height // (binsz**2)
+        num_samples = win_width * win_height // (binsz ** 2)
 
         I, J = np.meshgrid(
             np.arange(0, win_height, binsz), np.arange(0, win_width, binsz)
@@ -439,19 +439,86 @@ class SDLWrapper:
 
 
 def init_args():
-    parser = argparse.ArgumentParser(prog='visualize', description='Process input arguments')
+    parser = argparse.ArgumentParser(
+        prog='visualize', description='Process input arguments'
+    )
 
     vectorfields = list(calculate.VectorField(None)._fields.keys())
 
-    parser.add_argument('--field', '-f', action='store', default='none', required=False, choices=vectorfields, help='specify vector field type')
-    parser.add_argument('--xcenter', '-xc', action='store', default=(np.sqrt(2) * 0.2), type=float, required=False, help='x coordinate for center of the vector field')
-    parser.add_argument('--ycenter', '-yc', action='store', default=0, type=float, required=False, help='y coordinate for center of the vector field')
-    parser.add_argument('--dtheta', action='store', default=0.5, type=float, required=False, help='dtheta argument for generating some vector fields')
-    parser.add_argument('--radius', action='store', default=0.1, type=float, required=False, help='radius argument for generating some vector fields')
-    parser.add_argument('--buffer', action='store', default=0.0025, type=float, required=False, help='buffer argument for generating some vector fields')
-    parser.add_argument('--drmax', action='store', default=0.5, type=float, required=False, help='maximum speed in r direction that vector field can have')
-    parser.add_argument('--heatmap', '-hm', action='store_true', required=False, help='generates theta heatmap if present')
-    parser.add_argument('--state', '-s', action='store', required=True, choices=['animate', 'simulate', 'follow', 'none'], help="'animate': arm is animated according to sin and cos function ignoring vector field, 'simulate': arm dynamics are simulated for the given vector field, 'follow': arm is animated to move end effector to clicked location, 'none': do nothing")
+    parser.add_argument(
+        '--field',
+        '-f',
+        action='store',
+        default='none',
+        required=False,
+        choices=vectorfields,
+        help='specify vector field type',
+    )
+    parser.add_argument(
+        '--xcenter',
+        '-xc',
+        action='store',
+        default=(np.sqrt(2) * 0.2),
+        type=float,
+        required=False,
+        help='x coordinate for center of the vector field',
+    )
+    parser.add_argument(
+        '--ycenter',
+        '-yc',
+        action='store',
+        default=0,
+        type=float,
+        required=False,
+        help='y coordinate for center of the vector field',
+    )
+    parser.add_argument(
+        '--dtheta',
+        action='store',
+        default=0.5,
+        type=float,
+        required=False,
+        help='dtheta argument for generating some vector fields',
+    )
+    parser.add_argument(
+        '--radius',
+        action='store',
+        default=0.1,
+        type=float,
+        required=False,
+        help='radius argument for generating some vector fields',
+    )
+    parser.add_argument(
+        '--buffer',
+        action='store',
+        default=0.0025,
+        type=float,
+        required=False,
+        help='buffer argument for generating some vector fields',
+    )
+    parser.add_argument(
+        '--drmax',
+        action='store',
+        default=0.5,
+        type=float,
+        required=False,
+        help='maximum speed in r direction that vector field can have',
+    )
+    parser.add_argument(
+        '--heatmap',
+        '-hm',
+        action='store_true',
+        required=False,
+        help='generates theta heatmap if present',
+    )
+    parser.add_argument(
+        '--state',
+        '-s',
+        action='store',
+        required=True,
+        choices=['animate', 'simulate', 'follow', 'none'],
+        help="'animate': arm is animated according to sin and cos function ignoring vector field, 'simulate': arm dynamics are simulated for the given vector field, 'follow': arm is animated to move end effector to clicked location, 'none': do nothing",
+    )
 
     return parser.parse_args()
 
@@ -541,9 +608,7 @@ def main():
 
                 # write diagnostic text to window
                 text = [
-                    [
-                        'State: simulate'
-                    ],
+                    ['State: simulate'],
                     [
                         f'x:       {x:6.3f}, y:       {y:6.3f}',
                     ],
